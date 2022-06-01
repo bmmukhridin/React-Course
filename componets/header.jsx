c6cfc00d733ab9b90d0fb4470e98b7fd9471b8a8
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-05-31 20:45:17 -0700

    Initial commit

---
+++ componets/header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function header() {
+  return <></>;
+}
+
+export default header;
---
+++ src/App.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+import header from "./componets/header";
+
+export default function App() {
+  return <div></div>;
+}
