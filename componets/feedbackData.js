068a7d6974d345732687e3ef44204623e51689c8
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-06-11 07:00:16 -0700

    some-changes

--- componets/feedbackData.js
+++ componets/feedbackData.js
@@ -1,27 +1,27 @@
 const FeedbackData = [
   {
     id: 1,
-    rating: 10,
+    rating: 8,
     text:
-      "Sarvinoz sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae."
+      "Most Starbucks charge $2.95 for grande Pike Place coffee, but one store in Huntington Beach CA (Brookhurst & Hamilton) charges $3.25 for same grande. Lovely place, great staff, but price a bit of a turn off…"
   },
   {
     id: 2,
-    rating: 9,
+    rating: 10,
     text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae."
+      "Good service, the employees are good at both English and Tagalog. Fast service and the environment are really clean and tidy. And I also appreciate the fact that they use paper straws to support the environment. 5 stars :>"
   },
   {
     id: 3,
-    rating: 8,
+    rating: 1,
     text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae."
+      "How can people drink the awfully burnt tasting coffee from Starbucks. Overpriced poor tasting. Better to go to Dunkin. Starbucks uses cheaply source beans and roasts them to death. Not only burn the beans, also burn the essential oils."
   },
   {
     id: 4,
     rating: 7,
     text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae."
+      "There were 6 barristers or helper at the counter at one time but the services was so poor. Not efficient, slow, not service oriented, no team work and they were not supporting each other. At the end, services to customers were affected."
   }
 ];
 
